688fe6748c4482fc3d656408bce9aad80c17c57d
Javier Brea <javier.brea@gmail.com>
2018-12-06 08:16:54 +0100

    Add Switch plugin Factory

---
+++ plugin/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { DOMAPIC } = require('../lib/statics')
+const SwitchFactory = require('../lib/plugins/SwitchFactory')
+
+module.exports = function (homebridge) {
+  const Switch = new SwitchFactory(homebridge.hap.Service, homebridge.hap.Characteristic)
+  homebridge.registerAccessory(DOMAPIC, `${DOMAPIC}${Switch.name}`, Switch)
+
+  // TODO, register accesories for all HomeKit accesory types
+}
