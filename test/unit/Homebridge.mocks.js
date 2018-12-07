e3dce3ed15d925a380ebfd4993994fa2449b7a39
Javier Brea <javier.brea@gmail.com>
2018-12-07 11:06:03 +0100

    Add Switch plugin unit tests

---
+++ test/unit/Homebridge.mocks.js
@@ -0,0 +1,54 @@
+const test = require('narval')
+
+const Mock = function () {
+  let sandbox = test.sinon.createSandbox()
+
+  const stubs = {
+    hap: {
+      Service: sandbox.stub(),
+      Characteristic: sandbox.stub()
+    },
+    registerAccessory: sandbox.stub()
+  }
+
+  const instances = {
+    accessoryInformation: {
+      setCharacteristic: sandbox.stub().callsFake(() => {
+        return instances.accessoryInformation
+      })
+    },
+    switch: {
+      on: sandbox.stub().callsFake(() => {
+        return instances.switch
+      }),
+      getCharacteristic: sandbox.stub().callsFake(() => {
+        return instances.switch
+      })
+    }
+  }
+
+  stubs.hap.Characteristic.Model = 'MODEL'
+  stubs.hap.Characteristic.SerialNumber = 'SERIAL_NUMBER'
+  stubs.hap.Characteristic.Manufacturer = 'MANUFACTURER'
+  stubs.hap.Characteristic.On = 'ON'
+
+  stubs.hap.Service.AccessoryInformation = sandbox.stub().callsFake(function () {
+    return instances.accessoryInformation
+  })
+
+  stubs.hap.Service.Switch = sandbox.stub().callsFake(function () {
+    return instances.switch
+  })
+
+  const restore = () => {
+    sandbox.restore()
+  }
+
+  return {
+    restore,
+    stubs,
+    instances
+  }
+}
+
+module.exports = Mock
--- test/unit/plugin/index.specs.js
+++ test/unit/plugin/index.specs.js
@@ -1,6 +1,7 @@
 const test = require('narval')
 
 const SwitchFactoryMocks = require('../lib/plugins/SwitchFactory.mocks')
+const HomebridgeMocks = require('../Homebridge.mocks')
 
 test.describe('plugin', () => {
   let sandbox
@@ -10,16 +11,10 @@ test.describe('plugin', () => {
 
   test.before(() => {
     sandbox = test.sinon.createSandbox()
-    homebridge = {
-      hap: {
-        Service: 'foo Service',
-        Characteristic: 'foo Characteristic'
-      },
-      registerAccessory: sandbox.stub()
-    }
+    homebridge = new HomebridgeMocks()
     switchFactory = new SwitchFactoryMocks()
     plugin = require('../../../plugin/index')
-    plugin(homebridge)
+    plugin(homebridge.stubs)
   })
 
   test.after(() => {
@@ -28,10 +23,10 @@ test.describe('plugin', () => {
   })
 
   test.it('should create a new Switch accessory', () => {
-    test.expect(switchFactory.stubs.Constructor).to.have.been.calledWith(homebridge.hap.Service, homebridge.hap.Characteristic)
+    test.expect(switchFactory.stubs.Constructor).to.have.been.calledWith(homebridge.stubs.hap.Service, homebridge.stubs.hap.Characteristic)
   })
 
   test.it('should register switch accesory in homebridge', () => {
-    test.expect(homebridge.registerAccessory.getCall(0).args[2]).to.equal(switchFactory.stubs.instance)
+    test.expect(homebridge.stubs.registerAccessory.getCall(0).args[2]).to.equal(switchFactory.stubs.instance)
   })
 })
