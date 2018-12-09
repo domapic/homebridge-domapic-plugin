d320c2e146dbb663ff5981034c497497b433a2b8
Javier Brea <javier.brea@gmail.com>
2018-12-09 09:27:53 +0100

    Add StatelessSwitch accessory. Set Homebdrige server name from service name. Set Homebridge server random mac, unique by plugin instance

--- plugin/index.js
+++ plugin/index.js
@@ -2,10 +2,14 @@
 
 const { DOMAPIC } = require('../lib/statics')
 const SwitchFactory = require('../lib/plugins/SwitchFactory')
+const StatelessSwitchFactory = require('../lib/plugins/StatelessSwitchFactory')
 
 module.exports = function (homebridge) {
   const Switch = new SwitchFactory(homebridge.hap.Service, homebridge.hap.Characteristic)
   homebridge.registerAccessory(DOMAPIC, Switch.name, Switch)
+
+  const StatelessSwitch = new StatelessSwitchFactory(homebridge.hap.Service, homebridge.hap.Characteristic)
+  homebridge.registerAccessory(DOMAPIC, StatelessSwitch.name, StatelessSwitch)
 
   // TODO, register accesories for all HomeKit accesory types
 }
